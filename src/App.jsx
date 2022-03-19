0d26a3fac1b94ef4cfc70e032b301974dc096dc5
josueisaihs <josueisaihs@gmail.com>
2022-03-19 01:22:55 -0400

    Start Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div className="container-fluid text-dark bg-light">
+      <h1 className='text-center'>TODO</h1>
+      <div className=''>        
+        <input type="text" placeholder='Crear tarea' className='form-control'  />
+      </div>      
+    </div>
+  );
+}
+
+export default App;
